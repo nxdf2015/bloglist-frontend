5f6c5a8397c949ad1a2881d5b7d57379a0dcad57
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-10-27 00:58:46 +0100

    part5 17-18

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,59 @@
-beforeEach(function(){
-  cy.request('get','http://localhost:3001/api/testing/reset')
-  cy.visit('http://localhost:3000/')
-})
+const user = { 'username':'hellas','name':'arto hellas', 'password' : 'secret' }
 
-describe('default display of the application',function(){
-  it('checking that the application displays the login form by default',function(){
+describe('Blog app', function () {
+
+  beforeEach(function () {
+    cy.request('get', 'http://localhost:3001/api/testing/reset')
+    cy.request('post','http://localhost:3001/api/users',user)
+
+
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('login form is shown by default', function () {
     cy.contains('login')
     cy.get('[data-test=login-form]')
   })
+
+  describe('login', function () {
+
+    it('succeeds with correct credentials', function () {
+      cy.get('[data-test=login-form]').as('form')
+      cy.get('@form').get('input[name=username').type(user.username)
+      cy.get('@form').get('input[name=password').type(user.password)
+
+      cy.get('@form').get('button[type=submit').click()
+      cy.contains(`${user.username} logged in`)
+    })
+
+    it('fails with wrong credentials', function() {
+
+      cy.get('[data-test=login-form]').as('form')
+      cy.get('@form').get('input[name=username').type(user.username)
+      cy.get('@form').get('input[name=password').type('invalid password')
+      cy.get('@form').get('button[type=submit').click()
+      cy.clock()
+      cy.contains('invalid credential').should('have.css' ,'color','rgb(255, 0, 0)')
+      cy.tick(2000)
+      cy.contains('log in to application')
+    })
+  })
+
+  describe.only('When logged in', function() {
+    beforeEach(function() {
+      cy.get('[data-test=login-form]').as('form')
+      cy.get('@form').get('input[name=username').type(user.username)
+      cy.get('@form').get('input[name=password').type(user.password)
+      cy.get('@form').get('button[type=submit').click()
+
+
+    })
+
+    it('A blog can be created', function() {
+
+      cy.get('button[data-test=show-form-blog]').click()
+      cy.contains('create new')
+    })
+  })
+
 })
--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -5,7 +5,7 @@ const Togglable = ({ children }) => {
   const [visible , setVisible ] = useState(false)
   return (<div className="container">
     { visible && children}
-    <button onClick={() => setVisible(state => !state)}>{visible ? 'cancel' : 'create blog' }</button>
+    <button data-test="show-form-blog" onClick={() => setVisible(state => !state)}>{visible ? 'cancel' : 'create blog' }</button>
   </div>)
 
 }
